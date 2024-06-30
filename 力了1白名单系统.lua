cd7a5f44383e49e8cb1c3eb479cbf8a7973f909d
dingding123hhh <168184738+dingding123hhh@users.noreply.github.com>
2024-06-30 13:42:05 +0800

    Add files via upload

---
+++ 力了1白名单系统.lua
@@ -0,0 +1,17 @@
+
+local whitu = false
+local Name = game.Players.LocalPlayer.Name
+
+if Name == "xszxwhbdd" then 
+whitu = true 
+elseif Name == "123fa98" then 
+whitu = true 
+elseif Name == "Roblox_OldSix666" then 
+whitu = true 
+end
+
+if whitu then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/Roblox/main/Script_Hub.lua"))()
+else
+    game.Players.LocalPlayer:Kick("你没有被加入白名单")
+end
